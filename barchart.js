576658fc901d7286de452be01a14182515c69c9c
vivimortensen <vivi4531@stud.kea.dk>
2021-02-11 16:07:23 +0100

    small adds

--- barchart.js
+++ barchart.js
@@ -1,1 +1,44 @@
 "use strict"; 
+
+window.addEventListener("DOMContentLoaded", modifyModel); 
+
+const barchart = [4,57,22,34,9,16,7,23,88,25,4,57,22,34,9,16,7,23,88,25,4,57,22,34,9,16,7,23,88,25,4,57,22,34,9,16,7,23,88,25]; 
+
+let barchartbar = document.querySelector("barchart");
+
+function init(){}
+
+function loop(){}
+
+function getNumberOfCustomers(){}
+
+function displayData(){}
+
+function modifyModel(){
+barchart.height = 500;
+console.log(modifyModel);
+
+}
+
+
+/*const arr = [];
+let counter = 0;
+
+function arrayCounter() {
+    //Add item in the beginning of the array with unshift
+  arr.unshift(counter);
+  //Add +1s
+  counter++;
+
+  //Array can not be longer than 9 items
+  //Extract item from the end of the array if the counter is bigger than 9
+  if (counter > 9) {
+    arr.pop(arr);
+  }
+
+  //Counter 1000 ms = 1 s
+  setTimeout(arrayCounter, 1000);
+
+  //Write the array in the console
+  console.log(arr);
+}*/
